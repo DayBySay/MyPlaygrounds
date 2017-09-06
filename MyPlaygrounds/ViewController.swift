74cb6798f4b015b3a975c1489a2b7c2f39308af6
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-09-06 21:01:52 +0900

    Initial Commit

---
+++ MyPlaygrounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPlaygrounds
+//
+//  Created by 清 貴幸 on 2017/09/06.
+//  Copyright © 2017年 daybysay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
