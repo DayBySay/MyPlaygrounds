446c9737598e352af79c88ec4d18eb4ca3456bb9
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-09-17 15:29:47 +0900

    ...

---
+++ Playgrounds/InstantiateFromClassObject.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Hoge {
+    let fuga = "piyo"
+}
+
+let className = "Hoge"
+
+//let cls: AnyClass = NSClassFromString(className)!
+
+//let instance = cls.alloc().init()
+
+//instance.fuga()
