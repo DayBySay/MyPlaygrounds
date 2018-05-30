f414bb92e085362a236860111c2402a385b7d129
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-05-30 18:53:07 +0900

    o...

--- Playgrounds/XOR.playground/Contents.swift
+++ Playgrounds/XOR.playground/Contents.swift
@@ -1,25 +1,64 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-//let x: Int8 = 0b00000010
-//let y: Int8 = 0b00000000
-//let z: Int8 = -1
+//let string = "a"
+//let character: Character = "a"
 //
-//let and = x & y
-//let or = x | y
-//let xor = x ^ y
-//let shift = z << 7
+//type(of: string)
+//type(of: character)
+//
+//
+//let a = "12345"
+//print(a.startIndex) // 0
+//print(a.endIndex) // 20
+//
+//print(a[a.startIndex]) // 1
+////print(a[a.endIndex]) // Fatal error: Can't form a Character from an empty String
+//
+//print(a[a.index(a.startIndex, offsetBy: 2)]) // 3
+//print(a[a.index(a.endIndex, offsetBy: -2)]) // 4
+import Foundation
+//
+//let options = String.CompareOptions.caseInsensitive
+//let order = "a".compare("a", options:options)
+//order == ComparisonResult.orderedDescending
 
-let plus: Int8 = 123
-let minus: Int8 = -123
+//let range1 = 1..<10
+//for i in range1 {
+//    print(i) // 1...9まで出力
+//}
+//type(of: range1) // CountableRange<Int>.Type
+//
+//let range2 = 1...10
+//for i in range2 {
+//    print(i) // 1...10まで出力
+//}
+//type(of: range2) // CountableClosedRange<Int>.Type
 
-let target = minus
-//let target = plus
 
-// 7bit 右シフトして符号ビットで埋まった値を取得する（0 or -1になる）
-let s = target >> 7
-// 右シフトした結果とXOR
-let i = target ^ s
-// 右シフトした結果をマイナス
-let abs = i - s // 123
+//let optionalDouble = Optional(1.0)
+//let optionalIsInfinite = optionalDouble?.isInfinite
+//
+//print(String(describing: optionalIsInfinite))
+
+//let t: (String, String) = ("a", "e")
+//
+//print(t)
+//
+//let nt = (a: "aa", a2: "ee")
+//print(nt.a)
+
+//let a: Any = 1
+//let isInt = a is Int
+
+//let any = "abc" as Any // upcast
+//let int "aaa" as Int // error
+
+//let any = 1 as Any
+//let int = any as? Int
+//let string = any as? String
+
+let any = 1 as Any
+let int = any as! Int
+//let string = any as! String
+
+
+
+
