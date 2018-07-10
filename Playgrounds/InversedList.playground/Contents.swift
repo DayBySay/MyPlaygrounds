c055173419a366a947b2b55ab1c9a60f770b5b93
Takayuki Sei <takayuki.sei@gree.net>
2018-07-11 01:05:01 +0900

    ...

--- Playgrounds/InversedList.playground/Contents.swift
+++ Playgrounds/InversedList.playground/Contents.swift
@@ -31,3 +31,19 @@ Calendar(identifier: .japanese).date(from: components)
 let date = Date()
 Calendar(identifier: .gregorian).component(.year, from: date)
 Calendar(identifier: .japanese).component(.year, from: date)
+
+let datestring = "2018-06-28T15:12:30+0900"
+let formatter = ISO8601DateFormatter()
+let d = formatter.date(from: datestring)!
+
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy'-'MM'-'dd'"
+dateFormatter.string(from: d)
+
+let currencyFormatter = NumberFormatter()
+currencyFormatter.usesGroupingSeparator = true
+currencyFormatter.numberStyle = .currency
+//currencyFormatter.locale = Locale(identifier: "ja_JP")
+let priceString = currencyFormatter.string(from: 9999)!
+print(priceString)
+
